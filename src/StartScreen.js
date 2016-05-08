903a4a941332ceaa4afcfb390f04cc7a6b82ed92
csilzen <silvacasandra@gmail.com>
2016-05-07 18:03:53 -0700

    add StartScreen component

---
+++ src/StartScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export class StartScreen extends Component {
+  render() {
+    return(
+      <div className='start-container'>
+        <img src='/static/credit_chase_logo.svg'/>
+      </div>
+    )
+  }
+}
